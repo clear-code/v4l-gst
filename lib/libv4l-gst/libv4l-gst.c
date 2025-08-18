184a0b03796054052cec45187d5b89671bde4bef
Takuro Ashie <ashie@clear-code.com>
2025-08-18 17:02:55 +0900

    Bump version number to distinguish with original one
    In addition add our copyright notice.

--- lib/libv4l-gst/libv4l-gst.c
+++ lib/libv4l-gst/libv4l-gst.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 Renesas Electronics Corporation
+ * Copyright (C) 2024-2025 ClearCode Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
